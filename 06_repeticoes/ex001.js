f8e8aece4743a89dd896188d30382683442c1acc
Samuel Nunes <161253667+Samuel-Nun3s@users.noreply.github.com>
2024-09-15 20:16:11 -0300

    while, do while

--- 06_repeticoes/ex001.js
+++ 06_repeticoes/ex001.js
@@ -1,10 +1,12 @@
 var c = 1;
 
+// Teste logico no começo
 while (c <= 30) {
     console.log(`Passo ${c}`);
     c ++;
 }
 
+// Teste logico no final
 do {
     console.log(`Passo ${c}`);
     c++;
