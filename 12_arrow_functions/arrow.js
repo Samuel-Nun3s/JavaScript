dba3b3f3725e92793538c62ae85ebe6e5af2b223
Samuel Nunes <161253667+Samuel-Nun3s@users.noreply.github.com>
2024-10-15 20:15:00 -0300

    Arrow Functions

---
+++ 12_arrow_functions/arrow.js
@@ -0,0 +1,20 @@
+// Função normal com 2 parametros
+function subtração(a, b) { 
+    return a - b;
+}
+
+// Arrow Function com 2 parametros
+function subtração(a, b) { 
+    return a - b;
+}
+
+// Função normal com 1 parametro
+function somar2(a) { 
+    return a + 2;
+}
+
+// Função normal com nenhum parametro
+function diaDoMes() { // Função normal com nenhum parametro
+    return new Date().getDate();
+}
+console.log(diaDoMes())
