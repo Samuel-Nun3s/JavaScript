0bae13f8cb8c25270048f98be9cb7a45defccd2c
Samuel Nunes <161253667+Samuel-Nun3s@users.noreply.github.com>
2024-10-16 18:58:36 -0300

    Arrow Functions

--- 12_arrow_functions/arrow.js
+++ 12_arrow_functions/arrow.js
@@ -4,17 +4,38 @@ function subtração(a, b) {
 }
 
 // Arrow Function com 2 parametros
-function subtração(a, b) { 
-    return a - b;
-}
+const subtração2 = (a, b) => a - b;
 
 // Função normal com 1 parametro
-function somar2(a) { 
+function somar(a) { 
     return a + 2;
 }
 
-// Função normal com nenhum parametro
-function diaDoMes() { // Função normal com nenhum parametro
+// Arrow Function com 1 parametro
+const somar2 = a => a + 2;
+
+// Função normal sem nenhum parametro
+function diaDoMes() {
     return new Date().getDate();
 }
-console.log(diaDoMes())
+console.log(diaDoMes());
+
+// Arrow Function sem nenhum parametro
+const diaDoMes2 = () => new Date().getDate();
+console.log(diaDoMes2());
+
+// Função normal com muitas funcionalidades
+function superFunçao(a, b) {
+    var subtracao = a - b;
+    subtracao -= 2;
+    var diaDoMes = new Date().getDate();
+    return diaDoMes;
+}
+
+// Arrow Function com muitas funcionalidades
+const superFunçao = (a, b) => {
+    var subtracao = a - b;
+    subtracao -= 2;
+    var diaDoMes = new Date().getDate();
+    return diaDoMes;
+}
