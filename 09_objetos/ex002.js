0273b35cc4c31047f0d6c9bf412d48ec61a23707
Samuel Nunes <161253667+Samuel-Nun3s@users.noreply.github.com>
2024-10-17 17:45:54 -0300

    Objetos

---
+++ 09_objetos/ex002.js
@@ -0,0 +1,8 @@
+const dadosAlfredo = {
+    nome: "Alfredo",
+    idade: 27,
+    endereco: "Rua que sobe e desce",
+    numeroIdentificador: 2135432
+};
+
+console.log(`Esses são os dados do cidadão: nome -> ${dadosAlfredo.nome}, idade -> ${dadosAlfredo.idade}, endereço -> ${dadosAlfredo.endereco}, NI -> ${dadosAlfredo.numeroIdentificador}`);
